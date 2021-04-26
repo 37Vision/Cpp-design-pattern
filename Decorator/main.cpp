1f5e0b5d8384203685b92645a108419f1cf49641
zhuohzhang <zhuohzhang@tencent.com>
2021-04-26 11:45:35 +0800

    重写装饰者模式

--- Decorator/main.cpp
+++ Decorator/main.cpp
@@ -14,16 +14,11 @@ int main()
     circle->set_desc();
     print(circle);
 
-    std::shared_ptr<Shape> red_circle = std::shared_ptr<RedDecorator>(
-        new RedDecorator(std::shared_ptr<Circle>(
-            new Circle)));
+    std::shared_ptr<Shape> red_circle = std::make_shared<RedDecorator>(std::make_shared<Circle>());
     red_circle->set_desc();
     print(red_circle);
 
-    std::shared_ptr<Shape> red_blue_rectangle = std::shared_ptr<BlueDecorator>(
-        new BlueDecorator(std::shared_ptr<RedDecorator>(
-            new RedDecorator(std::shared_ptr<Rectangle>(
-                new Rectangle)))));
+    std::shared_ptr<Shape> red_blue_rectangle = std::make_shared<BlueDecorator>(std::make_shared<RedDecorator>((std::make_shared<Rectangle>())));
     red_blue_rectangle->set_desc();
     print(red_blue_rectangle);
 }
