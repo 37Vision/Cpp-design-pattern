acafcf6b0b2238f346387bddcb956c945df2bedf
zhuohzhang <zhuohzhang@tencent.com>
2021-05-07 17:18:37 +0800

    注释抽象工厂

--- AbstactFactory/main.cpp
+++ AbstactFactory/main.cpp
@@ -1,4 +1,6 @@
 #include "factory_producer.hpp"
+
+//提供一个接口，让该接口负责创建一系列“相关或者相互依赖的对象”，无需指定它们具体的类。重点是一系列产品，即产品族，比如此例子的键盘和鼠标。
 int main(){
     auto af = FactoryProducer::get_factory("dell");
     if(af == nullptr)
